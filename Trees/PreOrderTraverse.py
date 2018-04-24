035d4d66c786f59d82b42af5336c1df9f91388bc
Shantanu1395 <Shantanu1395@gmail.com>
2018-04-24 11:51:44 +0530

    Create PreOrderTraverse.py

---
+++ Trees/PreOrderTraverse.py
@@ -0,0 +1,33 @@
+class Node(object):
+
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+
+class Tree():
+
+    def __init__(self):
+        self.root=None
+
+def PreOrder(root):
+    if root==None:
+        return
+    print(root.data,end=' ')
+    PreOrder(root.left)
+    PreOrder(root.right)
+                
+tree=Tree()
+tree.root=Node(1)
+
+tree.root.left=Node(-1)
+tree.root.right=Node(11)
+
+tree.root.left.left=Node(-2)
+tree.root.left.right=Node(-3)
+tree.root.right.left=Node(21)
+tree.root.right.right=Node(6)
+
+tree.root.left.right.right=Node(5)
+
+PreOrder(tree.root)

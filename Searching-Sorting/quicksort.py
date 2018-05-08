a9912d6daa62559b15f2f2812b0d27d645be3f17
Shantanu1395 <Shantanu1395@gmail.com>
2018-05-08 12:59:52 +0530

    Add files via upload

---
+++ Searching-Sorting/quicksort.py
@@ -0,0 +1,26 @@
+def partition(arr,low,high):
+    pivot=arr[high]
+    i=low
+    for j in range(low,high):
+        if arr[j]<=pivot:
+            temp=arr[i]
+            arr[i]=arr[j]
+            arr[j]=temp
+            i=i+1
+            
+            
+    temp=arr[high]
+    arr[high]=arr[i]
+    arr[i]=temp
+    
+    return i
+
+def quicksort(arr,low,high):
+    if low<high:
+        pivot=partition(arr,low,high)
+        quicksort(arr,low,pivot-1)
+        quicksort(arr,pivot+1,high)
+
+arr=[32,14,51,61,13,-4,76,43]
+quicksort(arr,0,len(arr)-1)
+print(arr)
